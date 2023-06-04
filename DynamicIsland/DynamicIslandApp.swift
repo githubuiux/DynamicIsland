55895bad0db02db2af80cb2756f27d8d691403c1
githubuiux <uiuxista@gmail.com>
2023-06-04 20:18:34 +0300

    Initial Commit

---
+++ DynamicIsland/DynamicIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandApp.swift
+//  DynamicIsland
+//
+//  Created by Levent on 4.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
